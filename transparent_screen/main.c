5206275cb5dfb646aab7d159eb72a157569e998f
Kyungho Won <sleep0712@hotmail.com>
2015-11-01 01:00:15 -0800

    transparent screen

---
+++ transparent_screen/main.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <gtk/gtk.h>
+#include <gdk/gdkscreen.h>
+#include <cairo.h>
+
+int main(int argc, char *argv[])
+{
+	char *buf;
+	while(1)
+	{
+		buf = (char *)malloc(100);
+		printf("kyungho bash@ubuntu:~$ ");
+		fgets(buf, 100, stdin);
+		// fgets adds '\n' end of the string.
+		buf[strlen(buf)-1] = '\0';
+		if(strcmp(buf, "exit") == 0)
+		{
+			break;
+		}
+		system(buf);
+	}
+	return 0;
+}
