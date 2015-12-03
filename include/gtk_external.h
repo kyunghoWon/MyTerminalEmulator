20458ffd26f7e9a215e6534571c2686cefc2adfc
Kyungho Won <sleep0712@hotmail.com>
2015-12-03 14:15:39 +0900

    design event handlers

--- include/gtk_external.h
+++ include/gtk_external.h
@@ -2,5 +2,7 @@
 #define _GTK_EXTERNAL_H_
 #include <gtk/gtk.h>
 void clicked(GtkWindow *win, GdkEventButton *event, gpointer user_data);
-
+void clicked2(GtkWindow *win, GdkEventButton *event, gpointer user_data);
+void clicked3(GtkWindow *win, GdkEventButton *event, gpointer user_data);
+void clicked4(GtkWindow *win, GdkEventButton *event, gpointer user_data);
 #endif
