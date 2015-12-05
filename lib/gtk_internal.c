b89ae3374a3856c03245e84bdea01c3a35848714
BeomD <bamjin204@gmail.com>
2015-12-05 19:45:47 +0900

    internal edit

--- lib/gtk_internal.c
+++ lib/gtk_internal.c
@@ -7,17 +7,12 @@
 
 #define GDK_F12 0xffc9
 #define GDK_F10 0xffc7
+
 #define GDK_bracketleft 0x05b
-#define GDK_bracketright 0x05d
-#define GDK_KEY_MASK 0xffe1
+
 #define GDK_braceleft 0x07b
-#define GDK_leftsinglequotemark 0xad0
-#define GDK_leftdoublequotemark 0xad2
 
 
-#define GDK_KEY_S 0x053
-#define GDK_KEY_I 0x049
-#define GDK_KEY_D 0x044
 
 struct my_gtk
 {
@@ -29,61 +24,93 @@ struct my_gtk
 };
 
 
-
 gboolean key_event(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
 {
 struct my_gtk *my = (struct my_gtk *)user_data;
 
-GtkTextMark *mark;
-GtkTextIter iter;
 
 switch (event->keyval)
- {	
-    case GDK_F12:
-      
-       gtk_widget_hide(my->file_one);
-       gtk_widget_hide(my->btns[0]);
-       gtk_widget_hide(my->btns[1]);
-       gtk_widget_hide(my->btns[2]);
-       gtk_widget_hide(my->btns[3]);
-      break;
-    case GDK_F10:
-       gtk_widget_show(my->file_one);
-       gtk_widget_show(my->btns[0]);
-       gtk_widget_show(my->btns[1]);
-       gtk_widget_show(my->btns[2]);
-       gtk_widget_show(my->btns[3]);
+ {
+       case GDK_F12:
+       screen_control(my,1);
+       break;
+     
+       case GDK_F10:
+        screen_control(my,2);     
        break;
 
-     case GDK_bracketleft:
-        mark = gtk_text_buffer_get_insert(my->file_two);
-        gtk_text_buffer_get_iter_at_mark(my->file_two, &iter, mark);
-
-
-	    gtk_text_buffer_insert(my->file_two, &iter, " ]", -1);
-        
+       case GDK_bracketleft:
+	edit_control(my,1);
 	break;
 
 	case GDK_braceleft:
-        mark = gtk_text_buffer_get_insert(my->file_two);
-        gtk_text_buffer_get_iter_at_mark(my->file_two, &iter, mark);
-        gtk_text_buffer_insert(my->file_two, &iter, " }", -1);
+	edit_control(my,2);
 	break;
+
 /*
-        case S:
-
-	break;
-
-	case GDK_I:
-
-	break;
-*/
 	default:
 		printf("key pressed: %s\n", gdk_keyval_name(event->keyval));
 		// if you want to handle keyval by name, use gdk_keyval_name
 	break;
+*/
+
+}
+
+}
+void screen_control(struct my_gtk *val,int i)
+{
+
+GtkTextMark *mark;
+GtkTextIter iter;
+switch (i)
+ {
+    case 1:
+
+       gtk_widget_hide(val->file_one);
+       gtk_widget_hide(val->btns[0]);
+       gtk_widget_hide(val->btns[1]);
+       gtk_widget_hide(val->btns[2]);
+       gtk_widget_hide(val->btns[3]);
+      break;
+     
+     case 2:
+       gtk_widget_show(val->file_one);
+       gtk_widget_show(val->btns[0]);
+       gtk_widget_show(val->btns[1]);
+       gtk_widget_show(val->btns[2]);
+       gtk_widget_show(val->btns[3]);
+       break;
+ }
 
 }
 
 
+void edit_control(struct my_gtk *my,int i)
+{
+
+GtkTextMark *mark;
+GtkTextIter iter;
+switch (i)
+ {
+        case 1:
+        mark = gtk_text_buffer_get_insert(my->file_two);
+        gtk_text_buffer_get_iter_at_mark(my->file_two, &iter, mark);
+	gtk_text_buffer_insert(my->file_two, &iter, " ]", -1);
+	break;
+
+	case 2:
+        mark = gtk_text_buffer_get_insert(my->file_two);
+        gtk_text_buffer_get_iter_at_mark(my->file_two, &iter, mark);
+        gtk_text_buffer_insert(my->file_two, &iter, " }", -1);
+
+	break;
+
+ }
 }
+
+
+
+
+
+
+
